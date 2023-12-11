8bb4755c72d5edabb9be3dc3fee577e707198f1f
JustinMitchell <mitchelljm04@gmail.com>
2023-12-11 15:25:55 -0500

    Navbar Setup

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import images from '../../constants/images'
 
 export const Navbar = () => {
   return (
-    <div>
-        
+    <div className='app__navbar'>
+        <div className='app__navbar_logo'>
+            <img src={images.logo}/>
+        </div>
+
+        <nav className='app__navbar'>
+            <div className='app__navbar_menu'>
+                <ul className='app__navbar_ul'>
+                    <li className='app__navbar_links'></li>
+                </ul>
+
+            </div>
+        </nav>
     </div>
   )
 }
